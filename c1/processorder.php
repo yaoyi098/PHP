78f287b25f5781de7ad488541002afa0dfb9656e
Yao Yimeng <yym900902@gmail.com>
2015-11-26 14:06:55 +0800

    2
    2

--- c1/processorder.php
+++ c1/processorder.php
@@ -2,9 +2,6 @@
 	$tireqty = $_POST['tireqty'];
 	$oilqty = $_POST['oilqty'];
 	$sparkqty = $_POST['sparkqty'];
-	$address = $_POST['address'];
-	$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
-	
 	
 	define('TIREPRICE', 100);
 	define('OILPRICE', 10);
@@ -18,13 +15,11 @@
 	<body>
 		<h1>YYM's Auto parts</h1>
 		<h2>Order Results</h2>
+		<p>Order processed.</p>
 		<?php
 			$totalqty = $tireqty + $oilqty + $sparkqty;
 			$totalamount = 0.00;
 			echo '<p>Order precessed at '.date('H:i, jS F Y').'</p>';
-			echo '<p>Your order is as follows: </p>';
-			echo 'Items ordered: '.$totalqty.'<br />';
-			
 			if ($totalqty == 0)
 			{
 				echo  '<p style="color:red">';
@@ -37,21 +32,18 @@
 							+ $oilqty * OILPRICE
 							+ $sparkqty * SPARKPRICE;
 				
-				if ($tireqty > 0)
-					echo $tireqty.'tires<br />';
-				if ($oilqty > 0)
-					echo $oilqty.'bottles of oil<br />';
-				if ($sparkqty > 0)
-					echo $sparkqty.'spark plugs<br />';
+				echo '<p>Your order is as follows: </p>';
+				echo $tireqty.'tires<br />';
+				echo $oilqty.'bottles of oil<br />';
+				echo $sparkqty.'spark plugs<br />';
 			}
+				
+				echo 'Items ordered: '.$totalqty.'<br />';
 				
 				echo 'Subtotal: $'.number_format($totalamount,2).'<br />';
 				$taxrate = 0.10;
 				$totalamount = $totalamount * (1 + $taxrate);
 				echo 'Total including tax: $'.number_format($totalamount,2).'<br />';
-				
-				echo "<p>Address to ship to is $address</p>";
-				
 			
 		?>
 	</body>
